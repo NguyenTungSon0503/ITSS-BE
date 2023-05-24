60a58c842ba4e7647ed92625f9b3746307bd9e1d
NguyenTungSon0503 <sonmeliodas0503@gmail.com>
2023-05-24 14:05:44 +0700

    update_env && expireTIme

--- utils/jwt-helpers.js
+++ utils/jwt-helpers.js
@@ -3,10 +3,10 @@ import jwt from "jsonwebtoken";
 function jwtTokens({ user_id, user_name, user_email }) {
   const user = { user_id, user_name, user_email };
   const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-    expiresIn: "20s",
+    expiresIn: "5m",
   });
   const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, {
-    expiresIn: "5m",
+    expiresIn: "15m",
   });
   return { accessToken, refreshToken };
 }
