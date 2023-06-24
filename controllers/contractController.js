bfd2a6e4971bae5043ebe376eb27a46dacc63a51
NguyenTungSon0503 <sonmeliodas0503@gmail.com>
2023-06-24 15:09:57 +0700

    fixDate of contracts

--- controllers/contractController.js
+++ controllers/contractController.js
@@ -1,5 +1,6 @@
 import decodedToken from "../middleware/decode.js";
 import pool from "../db.js";
+import fixDate from "../utils/date-helpers.js";
 
 const createContract = async (req, res) => {
   try {
@@ -38,7 +39,11 @@ const getContractUser = async (req, res) => {
       "SELECT c.id,i.start_time,i.end_time,i.date, u.name, u.sex, u.age, u.avatar, r.food_recommend, r.meal_price, r.description FROM contracts c INNER JOIN recommendations r on c.recommendation_id = r.id INNER JOIN invitations i ON r.invitation_id = i.id INNER JOIN users u ON r.recommendation_sender_id = u.id WHERE i.invitation_sender_id = $1;",
       [user_id]
     );
-    res.send(getContractsInfo.rows);
+    const contractsInfo = getContractsInfo.rows
+    contractsInfo.map((contract) => {
+      contract.date = fixDate(contract.date);
+    });
+    res.send(contractsInfo);
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: error.message });
@@ -60,7 +65,11 @@ const getContractPartner = async (req, res) => {
       "SELECT c.id,i.start_time,i.end_time,i.date, u.name, u.sex, u.age, u.avatar, r.food_recommend, r.meal_price, r.description FROM contracts c INNER JOIN recommendations r ON c.recommendation_id = r.id INNER JOIN invitations i ON r.invitation_id = i.id INNER JOIN users u ON i.invitation_sender_id = u.id WHERE r.recommendation_sender_id = $1;",
       [user_id]
     );
-    res.send(getContractsInfo.rows);
+    const contractsInfo = getContractsInfo.rows
+    contractsInfo.map((contract) => {
+      contract.date = fixDate(contract.date);
+    });
+    res.send(contractsInfo);
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: error.message });
