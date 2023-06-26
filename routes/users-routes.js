b648b6b307ace6d3ab8063a27bb22f5b7da4f3b4
NguyenTungSon0503 <sonmeliodas0503@gmail.com>
2023-06-27 00:15:19 +0700

    update profile API docs

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -13,12 +13,110 @@ router.use(express.json());
 /* GET users listing. */
 router.get("/", authenticateToken, getUsers);
 
-// dang ky user, tra ve token
+// Register user and return token
 router.post("/", createUser);
+/**
+ * @swagger
+ * /api/users/update:
+ *   post:
+ *     description: Update user profile
+ *     parameters:
+ *       - name: authorization
+ *         in: header
+ *         description: Bearer token
+ *         required: true
+ *         schema:
+ *           type: string
+ *           format: uuid
+ *       - name: accessToken
+ *         in: cookie
+ *         description: Access token
+ *         required: true
+ *         schema:
+ *           type: string
+ *           format: uuid
+ *       - name: name
+ *         description: name
+ *         in: formData
+ *         required: true
+ *         type: string
+ *       - name: nation
+ *         description: nation
+ *         in: formData
+ *         required: true
+ *         type: string
+ *       - name: location
+ *         description: location
+ *         in: formData
+ *         required: true
+ *         type: string
+ *       - name: sex
+ *         description: sex
+ *         in: formData
+ *         required: true
+ *         type: string
+ *       - name: age
+ *         description: age
+ *         in: formData
+ *         required: true
+ *         type: string
+ *       - name: bio
+ *         description: bio
+ *         in: formData
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: integer
+ *                   format: i
+ *                 email:
+ *                   type: string
+ *                 password:
+ *                   type: string
+ *                   example: $2b$10$Rk8sGUJqy63HVn0t0
+ *                 role:
+ *                   type: string
+ 
+ *                 name:
+ *                   type: string
+ * 
+ *                 nation:
+ *                   type: string
+ *  
+ *                 location:
+ *                   type: string
+ *
+ *                 sex:
+ *                   type: strin
+ *                 age:
+ *                   type: integer
+ *                   format: in
+ *                 bio:
+ *                   type: strin
+ *                 rating:
+ *                   type: [number, null]
+ *                 avatar:
+ *                   type: string
+ *                 
+ *                 created_at:
+ *                   type: string
+ *                   format: date-time
+ *                  
+ *                 updated_at:
+ *                   type: string
+ *                   format: date-time
+ *                  
+ */
 
 router.post("/update", authenticateToken, updateProfile);
 
-// xoa toan bo users
+// Delete all users
 router.delete("/", deleteUsers);
 
 export default router;
