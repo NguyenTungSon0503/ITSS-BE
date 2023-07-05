56f861f69d0e0d189b2b0d8a5f39fe9a549bf7af
NguyenTungSon0503 <sonmeliodas0503@gmail.com>
2023-07-05 18:11:53 +0700

    chat

--- app.js
+++ app.js
@@ -28,6 +28,8 @@ app.use(cookieParser());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
+
+
 app.use("/", express.static(join(__dirname, "public")));
 app.use("/api/images", imageRouter);
 app.use("/api/auth", authRouter);
